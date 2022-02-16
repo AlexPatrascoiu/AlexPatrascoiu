37a9f941492a1efaafbe21b35066330c36881e21
Alex <alecs.patrascoiu@yahoo.ro>
2022-02-16 10:53:57 +0200

    corectie_1

--- PycharmProjects/tema/tema#3.py
+++ PycharmProjects/tema/tema#3.py
@@ -1,12 +1,12 @@
 #1 Declara o lista note_muzicale in care sa pui do re mi etc pana la do
-#
+
 # note_muzicale = ['Do','Re','Mi','Fa','Sol','La','Si','Do']  #am creat o lista
 # print(note_muzicale)                                        #am afisat o lista
 # reversed = note_muzicale[::-1]                              #am inversat lista folosind slice
 # print(reversed)                                             #am printat lista inversata
-# re_reversed = note_muzicale[::1]                            #am reinversat lista inversata tot cu slice (alta metoda nu stiu)
+# re_reversed = note_muzicale.reverse()                        #am reinversat lista inversata tot cu slice (alta metoda nu stiu)
 # print(re_reversed)                                          #am afisat-o
-#
+
 # #2 De cate ori apare ‘do’?
 #
 # print(note_muzicale.count('Do'))                           #am afisat de cate ori apare nota Do cu functia COUNT
@@ -19,10 +19,13 @@
 # list2 = [6,5,4]
 # list3 = list1 + list2                           #creez o lista noua.
 # list3.sort()                                    #cu functia .sort am sortat noua lista
-# print(list3)
-#
+# print(list3)                                    #afisez lista in ordine numerica
 # #varianta 2
-# print(list1[2],list1[1],list1[3],list1[0],list2[2],list2[1],list2[0])
+# print(list1[2],list1[1],list1[3],list1[0],list2[2],list2[1],list2[0])  #am creat o ordina numerica fortata folosind indexii listelor.
+# #varianta 3
+# list1.extend(list2)
+# print(list1)
+
 
 #4 Sortati si afisati lista generata la ex anterior
 # print(list3)                                           #am afisat lista generata anterior
@@ -51,13 +54,14 @@
 
 #8 Avand dictionarul dict1 = {'Ana' : 8, 'Gigel' : 10, 'Dorel' : 5} Folositi o functie ca sa afisati Elevii (cheile)
 
-dict1 = {'Ana' : 8,'Gigel' : 10,'Dorel' : 5}
-
-# print(dict1.keys())
+# dict1 = {'Ana' : 8,'Gigel' : 10,'Dorel' : 5}
 #
+# # print(dict1.keys())
+# #
 # #9 Printati cei 3 elevi si notele lor
 #
-# print(dict1)
+# dict2 = dict1.keys()
+# print(dict2)
 
 #10 Dorel a facut contestatie si a primit 6; Modificati nota lui Dorel in 6
 
@@ -78,19 +82,20 @@ dict1 = {'Ana' : 8,'Gigel' : 10,'Dorel' : 5}
 
 #13 Folositi un if si verificati daca: Weekend este un subset al zilelor din sapt sau Weekend nu este un subset al zilelor din saptamana.
 
-# if not zile_sapt == weekend :
-#     print('Weekendul este un subset al zileleor din sapt')
+# if  weekend.issubset(zile_sapt) :
+#     print('Weekendul este un subset al zilelor din sapt')
 # else :
 #     print('wekeendul nu este un subset al zilelor din sapt')
-#
-# #14 Afisati diferentele dintre aceste 2 seturi
+
+
+#14 Afisati diferentele dintre aceste 2 seturi
 
 # print(zile_sapt.difference(weekend))                    #afisez diferenta dintre o saptamana intraega si weekend
 
 #15 Afisati intersectia elementelor din aceste 2 seturi
 
 # print(zile_sapt.intersection(weekend))                  #afisez zilele comune dintre 2 seturi
-#
+
 
 
 
